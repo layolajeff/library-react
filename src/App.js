131322de822b722f28117cda92a25efd10682034
layolajeff <layolaj@yahoo.com>
2025-07-19 09:49:54 -0700

    added dicount and library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 /* import './App.css'; */
+import Discounted from './components/Discounted';
+import Explore from './components/Explore';
 import Featured from './components/Featured';
 import Highlights from './components/Highlights';
 import Landing from './components/Landing';
@@ -12,6 +14,8 @@ function App() {
       <Landing />
       <Highlights />
       <Featured />
+      <Discounted />
+      <Explore />
     </div>
   );
 }
