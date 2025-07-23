9c995a38ca267c2a33853d7477cad1bda1bbc22c
layolajeff <layolaj@yahoo.com>
2025-07-23 09:56:02 -0700

    Added Sorting Feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Books from './pages/Books';
+import { books } from "./data";
 
 
 function App() {
@@ -12,8 +13,7 @@ function App() {
       <div className="App">
         <Nav />
         <Route path="/" exact component={Home} />
-        <Route path="/books" component={Books} />
-        <Home />
+        <Route path="/books" render={() => <Books books={books} />} />
         <Footer />
       </div>
     </Router>
