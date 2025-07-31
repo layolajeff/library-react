33f1138938d74911041e50cf2e7029253a8caf1b
layolajeff <layolaj@yahoo.com>
2025-07-30 17:39:36 -0700

    Refactored explore section

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -2,17 +2,7 @@ import React from 'react';
 
 const Explore = () => {
     return (
-        <section id="explore">
-            <div className="container">
-                <div className="row row__column">
-                    <h2>
-                        Explore more <span className="purple">Books</span>
-                    </h2>
-                    <a href="/books">
-                    <button className="btn">Explore books</button></a>
-                </div>
-            </div>
-        </section>
+      
     );
 }
 
